6ee1f8e1e31730edc4b27a4585cd52b63af3e508
Rezhwan Sdeq <rezhwansidiq@gmail.com>
2024-02-28 17:47:57 +0300

    Adding [error, notfound, loading]

---
+++ src/app/mot-found.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h2>NotFound</h2>
+      <Link href="./">Return Home</Link>
+    </div>
+  );
+};
+export default NotFound;
